f02781407aeb96c182771f884540d47567333484
settaoui98 <hamzasitaoui@gmail.com>
2023-07-17 16:52:41 -0700

    UP

---
+++ 0x0C-python-almost_a_circle/test_models/test_rectangle.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+"""unittests for rectangel class"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """define tests for class rectangle"""
+    pass
---
+++ 0x0C-python-almost_a_circle/test_models/test_square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""unittests for cass Square"""
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """define unittests for class Square"""
+    pass
